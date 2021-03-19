1f48fa89902c781dcf26204ce0d1fa9881a97a00
Alexander Wilson <awlego@gmail.com>
2021-03-18 22:47:35 -0700

    new poem

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,45 +2,50 @@ var store = [{
         "title": "2019 Big 5 Personality Test",
         "excerpt":"I’m interested in tracking my personality test scores over time.   My scores on the “Big 5 Personality Test” from Truity.com:     Openness: 85%   Conscientiousness: 54%   Extraversion: 46%   Agreeableness: 75%   Neuroticisim: 2%  ","categories": ["About Me"],
         "tags": ["personality","self"],
-        "url": "/about%20me/big-5-personality-2019/",
+        "url": "http://localhost:4000/about%20me/big-5-personality-2019/",
         "teaser":null},{
         "title": "2020 Big 5 Personality Test",
         "excerpt":"I’m interested in tracking my personality test scores over time. Here are my results after the end of year 2020 and the changes from the same test I did last year: My scores on the “Big 5 Personality Test” from Truity.com: Openness: 87.5% (+2.5%) Conscientiousness: 56% (+2%) Extraversion: 54% (+8%)...","categories": ["About Me"],
         "tags": ["personality","self"],
-        "url": "/about%20me/big-5-personality-2020/",
+        "url": "http://localhost:4000/about%20me/big-5-personality-2020/",
         "teaser":null},{
         "title": "Lights",
         "excerpt":"Lights June is an endless orgy of light December is a seductive dance of color Life is harsh but beautiful. Beauty is full here. Full. It has to be, because life is harsh. A poem I wrote while traveling above the article circle in Norway with my grandparents. I don’t...","categories": ["Poems"],
         "tags": ["poem"],
-        "url": "/poems/poem.lights/",
+        "url": "http://localhost:4000/poems/poem.lights/",
         "teaser":null},{
         "title": "36",
         "excerpt":"36 Breaks of silence hold the room Muffling the cranking inside their heads Until courage rises and breath Solidifies history, hopes, and Being Old oak shelters from the Glory of brilliant lights above But holds tight the warmth they share From questions answered, and bodies laid bare. A poem I...","categories": ["Poems"],
         "tags": ["poem"],
-        "url": "/poems/poem.36/",
+        "url": "http://localhost:4000/poems/poem.36/",
         "teaser":null},{
         "title": "Believers",
         "excerpt":"Believers Dark heels clicked striking the cobblestone; confidence. A shared spell Spun with warmth; finger tips in night air He shouldered dark threads spun crisp and clean, accents, bold. She draped white soft with gold flowing like rivers old He was iron a soft strength, pliable with furnace. She was...","categories": ["Poems"],
         "tags": ["poem"],
-        "url": "/poems/poem.believers/",
-        "teaser":null},{
-        "title": "Deathbed (rough)",
-        "excerpt":"Deathbed (rough) Late nights waiting By the hospital bed cold and bright Warm drip drip Of the morphine Putting up a fight Wandering halls alone with old friends Here in the light Stories shared and known We cried, raged, and cried B-ball games so tight I’m pretty sure I wrote...","categories": ["Poems"],
-        "tags": ["poem"],
-        "url": "/poems/poem.cunning-linguist-copy/",
+        "url": "http://localhost:4000/poems/poem.believers/",
         "teaser":null},{
         "title": "Cunning Linguist",
         "excerpt":"Cunning Linguist An aspiring cunning linguist The man practiced cunnilingus His poems penned with swirling tongue Until his song her lips had sung It’s silly and maybe crass, but I’m still amused. The first two lines came to me while on the last leg of a run. I was climbing...","categories": ["Poems"],
         "tags": ["poem"],
-        "url": "/poems/poem.cunning-linguist/",
+        "url": "http://localhost:4000/poems/poem.cunning-linguist/",
+        "teaser":null},{
+        "title": "Deathbed (rough)",
+        "excerpt":"Deathbed (rough) Late nights waiting By the hospital bed cold and bright Warm drip drip Of the morphine Putting up a fight Wandering halls alone with old friends Here in the light Stories shared and known We cried, raged, and cried B-ball games so tight I’m pretty sure I wrote...","categories": ["Poems"],
+        "tags": ["poem"],
+        "url": "http://localhost:4000/poems/poem.deathbed/",
         "teaser":null},{
         "title": "Music of the Ear",
         "excerpt":"Music of the Ear   a kiss  a wish  a blink of the eye  a beat  a bump  a piece of the pie  a ring  a king  a crash of the sky  a tear  a stare  a query of the why     I like the rhythm of this.  ","categories": ["Poems"],
         "tags": ["poem"],
-        "url": "/poems/poem.music-of-the-ear/",
+        "url": "http://localhost:4000/poems/poem.music-of-the-ear/",
+        "teaser":null},{
+        "title": "Lights Are Off",
+        "excerpt":"Lights Are Off The lights are off now As I tell you my fears I’m naked and afraid And Sharing makes it better. You clutch my shoulder As your worries seep out A warmth in the darkness Certainty… for now. The lights are off now And there is love to...","categories": ["Poems"],
+        "tags": ["poem"],
+        "url": "http://localhost:4000/poems/poem.lights-are-off/",
         "teaser":null},{
         "title": "Date Me",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/dateme/",
+        "url": "http://localhost:4000/dateme/",
         "teaser":null}]
